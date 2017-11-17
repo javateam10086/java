290e49cb17a554d4354a9197c6637214fb5c0315
ghjhhyuyuy <798389920@qq.com>
2017-11-17 21:36:06 +0800

    Add files via upload

---
+++ DataIOText.java
@@ -0,0 +1,19 @@
+package team;
+
+import java.util.ArrayList;
+
+public class DataIOText implements DataIO{
+
+	@Override
+	public ArrayList<?> getScoreFromText(String a) {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+	@Override
+	public String saveDataInText(ArrayList<?> list) {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+}
